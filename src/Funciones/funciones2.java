c81f1cc97d3e7018b862202f1d81913fc4d883ad
Alex72005 <asm0125@alu.medac.es>
2024-10-18 20:10:34 +0200

    cosas pablo fuera

---
+++ src/Funciones/funciones2.java
@@ -0,0 +1,13 @@
+package Funciones;
+public class funciones2 {
+    public static void main(String[] args) {
+        String frase = "castillo amarillo";
+        String nuevaFrase = frase.replace('a', '-');
+        System.out.println(nuevaFrase);
+
+        
+        
+
+    }
+    
+}
