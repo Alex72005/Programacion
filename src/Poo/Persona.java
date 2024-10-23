323bc560ed627283037732af0d8a0325378b10fa
Alex72005 <asm0125@alu.medac.es>
2024-10-23 19:07:01 +0200

    clases

--- src/Poo/Persona.java
+++ src/Poo/Persona.java
@@ -1,12 +1,38 @@
 package Poo;
 public class Persona {
     private String nombre;
-    private String fecha;
-    public Persona(String n, String f){
+    private int edad;
+
+    public Persona(String n,int e){
         this.nombre = n;
-        this.fecha = f;
+        this.edad = e;
     }
+    public Persona(){
+        this.nombre = "Javi";
+        this.edad = 19;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nom){
+        this.nombre = nom;
+
+    }
+    public void setEdad(int e){
+       this.edad = e;
+    }
+
+    public int getEdad(){           
+        return this.edad;
+    }
+
     public String toString(){
-        return this.nombre + " " + this.fecha; 
+        return "Edad: " + this.edad;
     }
 }
+
+
+
+
+
