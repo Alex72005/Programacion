03320820ea8b54534e3f70481c2d42bf81753db4
Alex72005 <asm0125@alu.medac.es>
2024-11-05 20:22:50 +0100

    reloj

--- src/Tarea8/src/Main.java
+++ src/Tarea8/src/Main.java
@@ -1,20 +1,13 @@
 package Tarea8.src;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         Reloj reloj = new Reloj();
-        Timer timer  = new Timer();
 
-        TimerTask tarea = new TimerTask() {
-            public void run(){
-                reloj.incrementar();
-                System.out.println(reloj.toString());
-            }
-        };
-        
-        timer.scheduleAtFixedRate(tarea, 0, 1000);
+        while (true) {
+            System.out.println(reloj);
+            reloj.incrementar();
+            Thread.sleep(1000);
+        }
     }
 }
