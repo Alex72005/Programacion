dbaad7666e3c751aa8aacbf9a2bf2d316fab16c8
Alex72005 <asm0125@alu.medac.es>
2024-11-20 13:16:19 +0100

    sistemaVentas

--- src/Poo/SistemaDeVentas/Main.java
+++ src/Poo/SistemaDeVentas/Main.java
@@ -1,10 +1,33 @@
 package Poo.SistemaDeVentas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Empleado e = new Empleado();
-        Gerente g = new Gerente();
+        List<Empleado> empleados = new ArrayList<>();
 
-        System.out.println(g.dirigirReunion());
+        Empleado e = new Empleado("Juan", 40, 1000);
+        Gerente g = new Gerente("María", 30, 2000, "Recursos Humanos");
+        Vendedor v = new Vendedor("Pedro", 20, 1500, 4000);
+
+        empleados.add(e);
+        empleados.add(g);
+        empleados.add(v);
+
+        for (int i = 0; i < empleados.size(); i++) {
+            Empleado emp = empleados.get(i);
+            System.out.println(emp.toString() + ", bonificacion: " + emp.calcularBonificacion());
+            
+            if (emp instanceof Gerente gerente) {
+                gerente.dirigirReunion();
+            }
+            else if (emp instanceof Vendedor vendedor) {
+                vendedor.vender(1000);
+                System.out.println("Después de una nueva venta: " + vendedor.toString() + " y la bonificación nueva es: " + vendedor.calcularBonificacion());
+            }
+            
+        }
+       
     }
 }
--- src/Poo/SistemaDeVentas/Vendedor.java
+++ src/Poo/SistemaDeVentas/Vendedor.java
@@ -1,5 +1,42 @@
 package Poo.SistemaDeVentas;
 
-public class Vendedor {
-    
+public class Vendedor extends Empleado {
+    private double ventas;
+
+    public Vendedor(){
+        super();
+        this.ventas = 0;
+    }
+
+    public Vendedor(String n, int e, double s, double v){
+        super(n, e, s);
+        this.ventas = v;
+    }
+
+    public Vendedor(Vendedor copia){
+        super(copia);
+        this.ventas = copia.ventas;
+    }
+
+    public double getVentas() {
+        return this.ventas;
+    }
+
+    public void setVentas(double ven) {
+        this.ventas = ven;
+    }
+
+    @Override
+    public double calcularBonificacion(){
+        return (super.salario * 0.10 ) + (this.ventas * 0.05);
+    }
+
+    public void vender(double total){
+        this.ventas += total;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", ventas: " + ventas;
+    }
 }
