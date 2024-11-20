186c3fe52bd147c65ee4fa648622e5c02bf276e0
Alex72005 <asm0125@alu.medac.es>
2024-11-20 18:44:45 +0100

    prueba6

---
+++ src/pruebas/prueba6/src/Autobus.java
@@ -0,0 +1,72 @@
+package pruebas.prueba6.src;
+
+public class Autobus extends Vehiculo{
+    private double precioKilometro;
+    private double kilometrosInicio;
+    private double KilometrosFin;
+
+    public Autobus(){
+        super();
+        this.precioKilometro = 0;
+    }
+
+    public Autobus(String m, double p){
+        super(m);
+        this.precioKilometro = p;
+    }
+
+    public Autobus(Autobus copia){
+        super(copia);
+        this.precioKilometro = copia.precioKilometro;
+    }
+
+    public double getPrecioKilometro() {
+        return this.precioKilometro;
+    }
+
+    public void setPrecioKilometro(double pre) {
+        this.precioKilometro = pre;
+    }
+
+    public double getKilometrosInicio() {
+        return this.kilometrosInicio;
+    }
+
+    public void setKilometrosInicio(double ini) {
+        this.kilometrosInicio = ini;
+    }
+
+    public double getKilometrosFin() {
+        return this.KilometrosFin;
+    }
+
+    public void setKilometrosFin(double fin) {
+        KilometrosFin = fin;
+    }
+
+
+    @Override
+    public void alquilar(){
+        super.alquilar();
+        this.kilometrosInicio = 60;
+    }
+
+    @Override
+    public void devolver(){
+        super.devolver();
+        this.KilometrosFin = 200;
+    }
+
+    public double calcularPrecio(){
+        return (this.KilometrosFin - this.kilometrosInicio) * this.precioKilometro;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ "Autobus [precioKilometro=" + precioKilometro + ", kilometrosInicio=" + kilometrosInicio
+                + ", KilometrosFin=" + KilometrosFin + "]";
+    }
+
+    
+    
+}
