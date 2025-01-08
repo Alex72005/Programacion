563f419e566450c30fc0dcab573ae2533a4fe78f
Alex72005 <asm0125@alu.medac.es>
2025-01-08 16:50:37 +0100

    c

---
+++ src/Ficheros/src/App.java
@@ -0,0 +1,19 @@
+package Ficheros.src;
+
+import java.io.FileReader;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        FileReader fr = new FileReader("prueba.txt");
+
+        int dato = fr.read();
+
+        while (dato != -1) {
+            System.out.println((char)dato);
+            dato = fr.read();
+        }
+
+        fr.close();
+    }
+}
