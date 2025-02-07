69fca02b6a35269f79db52e54ffd60e600960cbc
Alex72005 <asm0125@alu.medac.es>
2025-02-07 17:35:38 +0100

    tarea 14

--- src/Tareas/Tarea_14/src/App.java
+++ src/Tareas/Tarea_14/src/App.java
@@ -1,9 +1,84 @@
 package Tareas.Tarea_14.src;
 
+import java.io.File;
 import java.util.TreeSet;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         TreeSet<Paciente> pacientes = new TreeSet<Paciente>();
+
+        pacientes.add(new Paciente(10, 1, "Cesar", "Gallego", "Celiaco"));
+        pacientes.add(new Paciente(10, 2, "Alberto", "Marcos", "Intolerante a la Lactosa"));
+        pacientes.add(new Paciente(11, 1, "Victor", "Romero", "Diabético"));
+        pacientes.add(new Paciente(11, 2, "Alvaro", "Villalba", "Vegano"));
+        pacientes.add(new Paciente(12, 1, "Rafa", "Luque", "Sin restricciones"));
+
+        DocumentBuilderFactory dbf;
+        DocumentBuilder db;
+        Document doc = null;
+        Element eRaiz = null;
+        try {
+            // Creacion XML
+            dbf = DocumentBuilderFactory.newInstance();
+            db = dbf.newDocumentBuilder();
+            doc = db.newDocument();
+
+            eRaiz = doc.createElement("pacientes");
+            doc.appendChild(eRaiz);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        for (Paciente p : pacientes) {
+            //Estructura del elemento XML
+            Element ePaciente = doc.createElement("paciente");
+
+            Element eHabitacion = doc.createElement("habitacion");
+            eHabitacion.appendChild(doc.createTextNode(Integer.toString(p.getHabitacion())));
+            ePaciente.appendChild(eHabitacion);
+
+            Element eCama = doc.createElement("cama");
+            eCama.appendChild(doc.createTextNode(Integer.toString(p.getCama())));
+            ePaciente.appendChild(eCama);
+
+            Element eNombre = doc.createElement("nombre");
+            eNombre.appendChild(doc.createTextNode(p.getNombre()));
+            ePaciente.appendChild(eNombre);
+
+            Element eApellidos = doc.createElement("apellidos");
+            eApellidos.appendChild(doc.createTextNode(p.getApellidos()));
+            ePaciente.appendChild(eApellidos);
+
+            Element eDieta = doc.createElement("dieta");
+            eDieta.appendChild(doc.createTextNode(p.getDieta()));
+            ePaciente.appendChild(eDieta);
+
+            eRaiz.appendChild(ePaciente);
+        }
+
+        try {
+            // clases necesarias finalizar la creacion del archivo xml
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("src/Tareas/Tarea_14/tarea_14.xml"));
+
+            transformer.transform(source, result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        System.out.println("Archivo generado: tarea_14.xml");
     }
 }
--- src/Tareas/Tarea_14/src/Paciente.java
+++ src/Tareas/Tarea_14/src/Paciente.java
@@ -1,10 +1,123 @@
 package Tareas.Tarea_14.src;
 
-public class Paciente {
-    private int habitación;
+public class Paciente implements Comparable<Paciente>{
+    private int habitacion;
     private int cama;
     private String nombre;
     private String apellidos;
-    
+    private String dieta;
 
+    public Paciente(int habitacion, int cama, String nombre, String apellidos, String dieta) {
+        this.habitacion = habitacion;
+        this.cama = cama;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dieta = dieta;
+    }
+
+    public int getHabitacion() {
+        return this.habitacion;
+    }
+
+    public void setHabitacion(int habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public int getCama() {
+        return this.cama;
+    }
+
+    public void setCama(int cama) {
+        this.cama = cama;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return this.apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDieta() {
+        return this.dieta;
+    }
+
+    public void setDieta(String dieta) {
+        this.dieta = dieta;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + habitacion;
+        result = prime * result + cama;
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        result = prime * result + ((apellidos == null) ? 0 : apellidos.hashCode());
+        result = prime * result + ((dieta == null) ? 0 : dieta.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Paciente other = (Paciente) obj;
+        if (habitacion != other.habitacion)
+            return false;
+        if (cama != other.cama)
+            return false;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        if (apellidos == null) {
+            if (other.apellidos != null)
+                return false;
+        } else if (!apellidos.equals(other.apellidos))
+            return false;
+        if (dieta == null) {
+            if (other.dieta != null)
+                return false;
+        } else if (!dieta.equals(other.dieta))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente [habitación=" + habitacion + ", cama=" + cama + ", nombre=" + nombre + ", apellidos="
+                + apellidos + ", dieta=" + dieta + "]";
+    }
+
+    @Override
+    public int compareTo(Paciente o) {
+        if (this.habitacion > o.habitacion) {
+            return 1;
+        } else if (this.habitacion < o.habitacion) {
+            return -1;
+        } else{
+            if (this.cama > o.cama) {
+                return 1;
+            } else if (this.cama < o.cama) {
+                return -1;
+            }else{
+                return 0;
+            }
+        }
+    }
 }
